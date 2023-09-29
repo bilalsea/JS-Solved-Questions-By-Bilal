f2389d2a1ec41d7cf148f93c51a62722026ae162
Muhammad Bilal <mbilalsea@gmail.com>
2023-09-29 23:09:09 +0500

    JavaScript solve Questions for Web 3.0

---
+++ app.js.js
@@ -0,0 +1,736 @@
+// --------- Chapter 01-------------
+// 1)
+// alert('firstName');
+// alert('lastName');
+// alert('email');
+// alert('phoneNum');
+// alert('password');
+
+// 2)
+// alert("You're learning JavaScript!");
+
+// 3)
+// alert("Pakistan Zindabad!");
+
+// --------- Chapter 02-------------
+// 1)
+// var firstName;
+
+// 2)
+// var firstName;
+// firstName = "Muhammad Bilal";
+
+// 3)
+var teamName = "Saheen";
+alert(teamName);
+
+// 4)
+var bestMan = "charlie";
+bestMan = "Bilal";
+
+// --------- Chapter 03-------------
+// 1)
+var caseQty;
+
+// 2)
+caseQty = 144;
+
+// 3)
+// var caseQty;
+// caseQty = 144;
+// var num= "9";
+
+// 4)
+// var sum;
+sum = caseQty + Number(num);
+
+// 5)
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+
+// --------- Chapter 04-------------
+// 1)
+var productCost = 3.45;
+
+// 2)
+var NameOfBand;
+
+// 3)
+// var merchTotal = 100;
+
+// 4)
+var firstName = "Muhammad";
+var lastName = "Bilal";
+var Comb = firstName +" "+ lastName;
+
+// --------- Chapter 05-------------
+// 1)
+// Mod %
+
+// 2)
+// var num = 20 % 6; Will be zero
+
+// 3)
+var largeNum = 1000 * 2000;
+
+// 4)
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal - shippingCharge;
+
+// 5)
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal / shippingCharge;
+
+// 6)
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal * shippingCharge;
+alert(orderTotal);
+
+// --------- Chapter 06-------------
+// 1)
+// x += 1;
+
+// 2)
+// --X;
+
+// 3)
+//  y has 50;
+
+// 4)
+// z has 49;
+
+// 5)
+// var newnum = num++;
+
+// 6)
+// var newnum = num++;
+
+// 7)
+// var newnum;
+// newnum++;
+
+
+// --------- Chapter 07-------------
+// 1)
+// value is 14
+
+// 2)
+// value is 24
+
+// 3)
+// value is 24
+
+// 4)
+// value is 18
+
+// 5)
+var calculatedNum = 2 + 2 * 4 + 10;
+
+// 6)
+var calculatedNum = 2 + 2 * 4 + 10;
+
+// 7)
+var calculatedNum = 4 / 2 * 4;
+
+// --------- Chapter 08-------------
+// 1)
+// value will be '22'
+
+// 2)
+// value will be '333'
+
+// 3)
+// Hello,Dolly
+
+// 4)
+var message = ("Pakistan" + "Zindabad");
+alart(message);
+
+// 5)
+var num = "2" + 2;
+
+// 6)
+var str1 = "hello";
+var str2 = "world";
+
+var comb = str1 + str2;
+
+
+
+// --------- Chapter 09-------------
+
+// 1)
+// var firstName = prompt("Enter first name". );
+
+// 2)
+// var CoutryName = prompt("Enter Country name","China" );
+
+// 3)
+// var CoutryName = prompt("Enter Country name");
+var defaultValue = "China";
+
+
+// --------- Chapter 10-------------
+// 1)
+// var city = "Karachi"
+// if (city = "Karachi") {
+// console.log("The City OF Lights")
+// }
+
+// 2)
+
+// var x=2, y=2;
+// if (x === y) {
+    // var z = +prompt("enter number"); }
+
+// 3)
+var ZipCode  = "10010";
+// if (ZipCode  = "10010") {
+    alart("karachi");
+// }
+alart("Please write correctly");
+
+// --------- Chapter 11-------------
+
+// 1)
+// if (x != y){
+    
+// 2)
+// if (x >= y){
+    
+    // 3)
+// if (x != y){
+// var a = y;
+
+
+// --------- Chapter 12-------------
+// 1)
+// if (x >= y){
+//     alert(x)
+// } else {
+//     alert(y);
+// }
+
+// 2)
+// switch(ourOperand){
+
+// case "+":
+// result = opr01 + opr02;
+// break;
+
+// case "-":
+// result = opr01 - opr02;
+// break;
+
+// case "*":
+// result = opr01 * opr02;
+// break;
+
+// case "/":
+// result = opr01 / opr02;
+// break;
+
+// case "%":
+// result = opr01 % opr02;
+// break;
+
+// case "square":
+// result = opr01 ** opr01;
+// break;
+// default:
+// result = "Error";
+
+// 3)
+// if (a === 10) {
+//     alert("a is 10");
+//   }else{
+//     alart("The correct value of a is  " + a);
+//   }  
+
+// 4)
+var a = prompt("enter a city name");
+// if (a === "karachi") {
+//     alert("yes it is karachi");
+//   }
+// else if (a === "Lahore") {
+    //     alert("yes it is Lahore");
+    //   }else{
+//     alart("Not to do better");
+//   }  
+
+
+// --------- Chapter 13-------------
+// 1)
+// if(a==b || c==d){
+    
+// 2)
+// if(a==b && c!==d){
+
+// 3)
+// if((name=="hamza" || name=="Arsalan") && age <= 60 ){
+
+// --------- Chapter 14-------------
+// 1)
+// if (password !== "") {
+//     if (password.length >= 5) {
+//        alert("ok");
+//      }else{
+//         alert("Password must be greater than 5 ");
+//      }
+//    }
+
+// 2)
+// trying
+
+// 3)
+// if (a === 1 && c === "Max") {
+//    alert("OK");
+//     }
+  
+// 4)
+// --------- Chapter 15-------------
+// 1)
+// var arr = [];
+
+// 2)
+// var arr = ["str"];
+
+// 3)
+//var alphabet = ["h","i","j","k"]
+// alart(alphabet[2]);
+
+// 4)
+// alphabet.lenght
+
+// --------- Chapter 16-------------
+// 1)
+// var arr = ["str"];
+// Array.push("str2");
+
+// 2)
+// 2.	var Alphabet=["h","i","j","k"]
+// Alphabet.pop();
+
+// 3)
+// 2.	var Alphabet=["h","i","j","k"]
+// Alphabet.push(3);
+
+// --------- Chapter 17 to 20-------------
+// 1)
+// for (var i = 0; i < 10; i++){
+
+// 2)
+// for (var i = 0; i < 12; i++){
+
+// 3)
+// for (var i = 0; i <= 4; i++){}
+
+// 4)
+// for (var k = 0; k < 100; i++){
+    // run 100 times
+
+// 5)
+// for (var i = 3; i > 0; i--){}
+
+// 6)
+// for (var i = 3; i > arr.length; i--){}
+
+// 7)
+// var flag = true;
+
+// 8)
+// for (var i = 0; i > pets.length; i++){}
+
+// 9)
+// for (var i = 0; i < 10; i++){
+    // if(i == 2){
+    //     alart("1");
+    //     break
+    // }
+
+// 10)
+// var matchFound = false;
+// for (var i = 0; i < list.length; i++) {
+//   if (userInput === list[i]) {
+//     alert("Match found");
+//     matchFound = true;
+//     break;
+//   }
+// };
+
+// 11)
+// var matchFound = false;
+// for (var i = 0; i < list.length; i++) {
+//   if (userInput === list[i]) {
+//     alert("Match found");
+//     matchFound = true;
+//     break;
+//   } else{
+//     alart("match isn't found");
+//   }
+// };
+
+// 12)
+// var firstArr = [“a”, “b”, “c”, “d”, “e”, “f”];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < firstArr.length; i++){
+
+    // for (var i = 0; i < secondArr,length; i++){
+    
+    // }
+    // }
+
+// --------- Chapter 21-------------
+1)
+var allLower = userInput.toLowerCase();
+
+2)
+var allLower = allLower.toLowerCase();
+
+3)
+var allUpper = allLower.toUpperCase();
+
+4)
+var allLower = userInput.toLowerCase();
+
+5)
+var allLower = userInput[1].toLowerCase();
+
+6)
+var allUpper = allLower.toUpperCase();
+alart(allUpper);
+
+7)
+var cityName = “kaRacHi”;
+var Upper = (cityName.slice(0,1)).toUpperCase();
+var allLower = cityName.slice(1).toLowerCase();
+cityName = Upper + allLower;
+alart(userInput);
+
+
+
+// --------- Chapter 22 to 25-------------
+
+1)
+var sameWords = "captain" ;
+var slicedWord = sameWords.slice(1,2);
+
+2)
+var strLength = sameWords.length;
+
+3)
+var animal = "elephant";
+var seg = animal.slice(2,4);
+
+4)
+var animal = "elephant";
+var strLength = sameWords.length;
+var seg = animal.slice(2,4);
+
+5)
+var animal = "elephant";
+var seg = animal.slice(1,animal.length - 3);
+
+6)
+indx contain 3,
+
+7)
+indx conatain 17,
+
+8)
+var indx = text.lastIndexOf("go");
+
+9)
+if(index ===index.charAt(indexNum)){
+
+10)
+var str = "abcde";
+str2 = str.charAt(2);
+
+11)
+var cha = text.charAt(10);
+
+12)
+var cha = input .charAt(5);
+
+14)
+if(str.charAt(3)=== "a"){
+
+15)
+for (var i =0; i<str.length; i++){
+	arr[i]= str.slice(i,i+1);
+}
+
+17)
+y = x.replace(\g'a'\, "z");
+
+
+
+// --------- Chapter 26-------------
+1)
+var rounded = Math.round(random);
+
+2)
+var roundNum  = Math.round(origNum);
+
+3)
+var roundNum  = Math.floor(origNum);
+
+4)
+var origNum = 0.5;
+var roundNum  = Math.floor(origNum);
+
+
+// --------- Chapter 27-------------
+1)
+Math.random()*50
+
+2)
+var genratedNum = Math.random()*50
+
+3)
+Math.random()*6
+
+4)
+Math.random()*2
+
+// --------- Chapter 28 & 29------------
+1)
+Number(string);
+
+2)
+var string  = "123";
+var num = Number(string);
+
+3)
+var numb  = Math.floor(string);
+
+4)
+typeOf(myNumber);
+
+5)
+numberVariable.toString();
+
+
+// --------- Chapter 30-------------
+1)
+var num = Math.random()
+var newNum = num.tofixed(4);
+newNum = newNum.toString();
+
+2)
+var num = Math.random()
+var newNum = num.tofixed(2);
+newNum = newNum.toString();
+newNum = Number(newNum);
+
+3)
+if(num === num.tofixed(2) && typeOf(num) === string){
+
+// --------- Chapter 31 to 34 -------------
+1)
+var dObj = new Date();
+
+2)
+var dObj = new Date();
+var dStr = dObj.tostring();
+
+3)
+var dObj = new Date();
+var d = dObj.getDay();
+
+4)
+var dObj = new Date();
+var d = dObj.getDay();
+var dayNames = ["Mon","Tue","wed","Thur","Fri","Sat","Sun",];
+alart(dayNames[d]);
+
+7)
+var dObj = new Date(1992);
+var d = dObj.getDay();
+ 
+
+8)
+var dObj = new Date(1980);
+var dObj2 = new Date();
+
+9)
+var dObj = new Date(2024);
+
+10)
+dObj.setMonth();
+
+11)
+dObj.setDay();
+
+// --------- Chapter 35 to 37-------------
+1)
+function concat(){
+alart("Hello World");
+}
+
+2)
+function askName (){
+var userName = prompt("Enter name");
+}
+
+3)
+function Caller(){
+alart("Hello World");
+Caller2();
+askName();
+}
+
+4)
+function askName (){
+var userName = prompt("Enter name");
+alart(userName);
+}
+
+5)
+function concat(a, b, c){
+}
+concat(ourVariable,2,"Hello");
+
+6)
+function concat(a, b){
+var c = a + B
+alart(c);
+}
+
+7)
+function concat(a, b, d){
+var c = a * b *d
+alart(c);
+}
+
+8)
+function avg(){
+var sum = arr.forEach(arrValue,ArrIndex){
+arrValue += arrValue;
+}
+return sum / arr.length;
+}
+
+9)
+function Sum(a, b){
+return a + b;
+}
+
+10)
+function avg(){
+var sum = arr.forEach(arrValue,ArrIndex){
+arrValue += arrValue;
+}
+return sum / arr.length;
+}
+
+11)
+function Sum(a, b){
+return a + b;
+}
+var sum = sum();
+
+13)
+function setaYear(){
+var dObj = new Date(2024);
+}
+
+15)
+var array = ['zaid','haris','raza','abubakar','hassan','hussain','fatima'];
+function presense (i){
+if(array.indexof(i)== Number){
+array.indexof(i);
+return true;
+}
+return false;
+}
+
+16)
+var str = "abcd"
+function repeating(){
+str.repeat(10);
+}
+
+17)
+var array = ['a','b','c','d','e'];
+function arrReverse(){
+array,reverse();
+}
+
+// --------- Chapter 38-------------
+
+1)
+function useOfLocal(){
+var LocalVariable = "abcd";
+}
+
+2)
+var globalVariable = ""bcd"
+function useOfLocal(){
+globalVariable = "abcd";
+}
+
+
+// --------- Chapter 39 & 40-------------
+
+1)
+// switch(a){
+
+// case "+":
+// result = opr01 + opr02;
+// break;
+
+// case "-":
+// result = opr01 - opr02;
+// break;
+
+// case "*":
+// result = opr01 * opr02;
+// break;
+
+// case "/":
+// result = opr01 / opr02;
+// break;
+
+// case "%":
+// result = opr01 % opr02;
+// break;
+
+// case "square":
+// result = opr01 ** opr01;
+// break;
+// default:
+// result = "Error";
+
+2)
+
+// switch(cityName){
+
+// case "Karachi":
+// alart("yes It is Karachi");
+// break;
+
+// case "Lahore":
+// alart("yes It is Lahore");
+// break;
+
+// case "quetta":
+// alart("yes It is quetta");
+// break;
+
+
+// default:
+// result = "your entered wrong city";
+
+
+
+
+
+
